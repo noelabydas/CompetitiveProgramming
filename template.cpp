3d09b6d280738028d718914ae6bf2831723fefb7
leonatwork <noelabydas@gmail.com>
2017-08-12 13:32:04 +0530

    added template.cpp

---
+++ template.cpp
@@ -0,0 +1,43 @@
+/***********************************************************
+
+ This is a c++ template, which contains few functions and
+its shortcuts which can be used in competitive programming
+
+Author: leonatwork(Noel Aby Das)
+
+**********************************************************/
+
+/*header files section*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*required pre-processor directives*/
+#define ll long long int
+#define sd(x) scanf("%d",&x)
+#define sld(x) scanf("%ld",&x)
+#define slld(x) scanf("%lld",&x)
+#define sf(x) scanf("%f",&x)
+#define slf(x) scanf("%lf",&x)
+#define sc(x) scanf("%c",&x)
+#define ss(x) scanf("%s",x)
+#define vi vector<int>
+#define vll vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define repi(i,in,fi) for (int i=in ; i<fi ; i++)
+#define repd(i,fi,in) for (int i=fi ; i>=in ; i--)
+#define mod 1000000007
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    /*
+        code goes here
+    */
+
+    return 0;
+}
